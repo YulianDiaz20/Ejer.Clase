34229a52c1aa50a3e90c2b2da04a1b1793589cbc
YulianDiaz <yulian.diaz-g@uniminuto.edu.co>
2025-02-26 14:25:59 -0500

    cargar

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,46 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        int a = 0;
+        Scanner dato = new Scanner (System.in);
+        Scanner dato1 = new Scanner (System.in);
+        do{
+        System.out.println("¿Que dia es hoy?");
+        String dia = dato.next();
+        switch (dia.toUpperCase()){
+            case "LUNES" : System.out.println("Mañana es Martes");
+            break;
+            case "MARTES" : System.out.println("Mañana es Miercoles");
+            break;
+            case "MIERCOLES" : System.out.println("Mañana es Jueves");
+            break;
+            case "JUEVES" : System.out.println("Mañana es Viernes");
+            break;
+            case "VIERNES" : System.out.println("Mañana es Sabado");
+            break;
+            case "SABADO" : System.out.println("Mañana es Domingo");
+            break;
+            case "DOMINGO" : System.out.println("Mañana es Lunes");
+            break;
+            default :
+                a=1;
+                System.out.println("Ingrese un dia valido");
+            break;
+
+        }}while(a==1);
+do{
+        System.out.println("¿Eres estudiante o profesor?");
+        String rol = dato1.next();
+        switch (rol.toUpperCase()) {
+            case "ESTUDIANTE" -> System.out.println("Listo para aprender");
+            case "PROFESOR" -> System.out.println("Listo para enseñar");
+            default -> {
+                a=2;
+                System.out.println("Ingresa una palabra valida");
+            }
+
+        }}while(a==2);
 
 
 
